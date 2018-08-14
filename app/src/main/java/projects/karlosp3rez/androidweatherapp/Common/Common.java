e089543f74af7a384143389d1732a042a0b70f5a
Carlos Perez <legomarth@hotmail.com>
2018-08-13 22:33:56 -0500

    Se crea el proyecto y se implementan las librerías a utilizar Se crea una API key desde la web de OpenWeatherMap, y se agrega al proyecto

---
+++ app/src/main/java/projects/karlosp3rez/androidweatherapp/Common/Common.java
@@ -0,0 +1,7 @@
+package projects.karlosp3rez.androidweatherapp.Common;
+
+public class Common {
+
+    //Se crea una API key desde la web de OpenWeatherMap
+    public static final String APP_ID = "981b35f51fb519dc2415d752c23fccf4";
+}
