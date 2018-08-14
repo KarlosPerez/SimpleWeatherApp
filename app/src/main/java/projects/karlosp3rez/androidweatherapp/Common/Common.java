fcd1b6ac23b27e1edd9e53135f03f8f496c5eeae
Carlos Perez <legomarth@hotmail.com>
2018-08-14 00:13:43 -0500

    1. Se diseña la UI de la actividad principal 2. Se programa inicialización de los servicios de localización, para obtener la latitud/logitud. 3. Se crea el adaptador para crear la lista de fragmentos que serán incluidos en el ViewPager 4. Se crea el fragment para mostrar el clima del dia actual 5. Se modifica los estilos para prevenir crash con el actionBar y se modifica el estilo en el manifest. 6. Se crean los colores personalizados con los que se va a trabajar en la app

--- app/src/main/java/projects/karlosp3rez/androidweatherapp/Common/Common.java
+++ app/src/main/java/projects/karlosp3rez/androidweatherapp/Common/Common.java
@@ -1,7 +1,10 @@
 package projects.karlosp3rez.androidweatherapp.Common;
 
+import android.location.Location;
+
 public class Common {
 
     //Se crea una API key desde la web de OpenWeatherMap
     public static final String APP_ID = "981b35f51fb519dc2415d752c23fccf4";
+    public static Location localizacion_Actual = null;
 }
--- app/src/main/java/projects/karlosp3rez/androidweatherapp/MainActivity.java
+++ app/src/main/java/projects/karlosp3rez/androidweatherapp/MainActivity.java
@@ -1,13 +1,118 @@
 package projects.karlosp3rez.androidweatherapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Looper;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+import java.util.Objects;
+
+import projects.karlosp3rez.androidweatherapp.Adapter.ViewPagerAdapter;
+import projects.karlosp3rez.androidweatherapp.Common.Common;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private CoordinatorLayout coordinatorLayout;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coordinatorLayout = findViewById(R.id.root_view);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        //Request permission
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            buildLocationRequest();
+                            buildLocationCallback();
+
+                            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                                    ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                return;
+                            }
+                            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+                            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        Snackbar.make(coordinatorLayout,"Permission Denied",Snackbar.LENGTH_LONG).show();
+                    }
+                }).check();
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setSmallestDisplacement(10.0f);
+    }
+
+    private void buildLocationCallback() {
+        locationCallback = new LocationCallback() {
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                Common.localizacion_Actual = locationResult.getLastLocation();
+
+                viewPager = findViewById(R.id.view_pager);
+                setupViewPager(viewPager);
+                tabLayout = findViewById(R.id.tabs);
+                tabLayout.setupWithViewPager(viewPager);
+
+                //Log
+                Log.d("Location",locationResult.getLastLocation().getLatitude()+"/"+
+                        locationResult.getLastLocation().getLongitude());
+            }
+        };
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(TodayWeatherFragment.getInstance(),"Today");
+        viewPager.setAdapter(adapter);
     }
 }
